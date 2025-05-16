dae942edf9acec4854c9037003fa61330f273719
Victor Theophilus <chukwuebukatheophilus58@gmail.com>
2025-05-16 10:18:52 +0100

    my node-express-course

---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,28 @@
+const getAllTasks = (req, res) => {
+  res.send('all items from the file')
+}
+
+const createTask = (req, res) => {
+  res.send('Create task')
+}
+
+const getTask = (req, res) => {
+  res.send('get single task')
+}
+
+const updateTask = (req, res) => {
+  res.send('Update task')
+}
+
+const deleteTask = (req, res) => {
+  res.send('delete task')
+}
+
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask
+}
