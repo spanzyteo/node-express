2bd27e4f5250e007b01f585e37f32e49d8846b9f
Victor Theophilus <chukwuebukatheophilus58@gmail.com>
2025-05-21 13:51:14 +0100

    complete task-manager-section

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,28 +1,47 @@
 const Task = require('../models/task')
-const getAllTasks = (req, res) => {
-  res.send('all items from the file')
-}
+const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-error')
 
-const createTask = async (req, res) => {
-  try {
+const getAllTasks = asyncWrapper(async (req, res) => {
+    const task = await Task.find({})
+    res.status(200).json({task})
+})
+
+const createTask = asyncWrapper(async (req, res) => {
     const task = await Task.create(req.body)
     res.status(201).json({task})
-  } catch (error) {
-    res.status(500).json({msg: error})
-  }
-}
+})
 
-const getTask = (req, res) => {
-  res.json({id: req.params.id })
-}
+const getTask =  asyncWrapper(async (req, res, next) => {
+    const {id: taskID} = req.params
+    const task = await Task.findOne({ _id: taskID })
+    if (!task) {
+      return next(createCustomError(`No task with id: ${taskID}`, 404))
+    }
+    res.status(200).json({ task })
+})
 
-const updateTask = (req, res) => {
-  res.send('Update task')
-}
+const updateTask = asyncWrapper(async (req, res, next) => {
+    const { id: taskID } = req.params
+    const task = await Task.findOneAndUpdate({ _id:taskID }, req.body, {
+      new: true,
+      runValidators: true
+    })
 
-const deleteTask = (req, res) => {
-  res.send('delete task')
-}
+    if (!task) {
+      return next(createCustomError(`No task with id: ${taskID}`, 404))
+    }
+    res.status(200).json({ task})
+})
+
+const deleteTask = asyncWrapper(async (req, res, next) => {
+    const {id: taskID} = req.params
+    const task = await Task.findOneAndDelete({ _id: taskID })
+     if (!task) {
+      return next(createCustomError(`No task with id: ${taskID}`, 404))
+    }
+    res.status(200).json({task})
+})
 
 
 module.exports = {
